005af91d422dff27d421493070693cfa3cd4181a
BerkeKzyl <kuzeyliberke@gmail.com>
2025-09-03 14:18:56 +0300

    basics of the project have been set up

---
+++ urun_yonetimi/graphql_core/schema.py
@@ -0,0 +1,20 @@
+import graphene
+import graphql_jwt
+
+
+class Query(graphene.ObjectType):
+    ping = graphene.String()
+
+    def resolve_ping(root, info):
+        return "pong"
+
+
+class Mutation(graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
---
+++ urun_yonetimi/product/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from account.models import CustomUser
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    image_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+class FavoriteProduct(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ['user', 'product']
+    
+    def __str__(self):
+        return f"{self.user.email} - {self.product.name}"
